e890e2efded52bb8ddd97664c5c689fbf2b36c47
戴鹏鹏 <pengpeng.dai@china.zhaogang.com>
2022-03-23 10:38:24 +0800

    fix: commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,48 @@
 module.exports = {
-  dest: 'dist'
+  dest: 'dist',
+  title: '此间少年',
+  description: '我的个人博客',
+  head: [
+    ['link', { rel: 'icon', href: '/icon.jpeg' }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  markdown: {
+    lineNumbers: false // 代码块显示行号
+  },
+  themeConfig: {
+    navbar:[ // 导航栏配置
+      {text: 'JavaScript', link: '/javascript/prototype/' },
+      {text: 'Vue', link: '/vue/'},
+      {text: 'React', link: '/react/'}      
+    ],
+    sidebar: {
+      '/javascript/': [
+        {
+          text: 'JavaScript',
+          children: [
+            {
+              text: '原型原型链',
+              link: '/javascript/prototype/'
+            },
+            {
+              text: '继承',
+              link: '/javascript/继承/'
+            }
+          ],
+        }
+      ],
+      '/interview/': [
+        {
+          text: '面试记录',
+          children: [
+            {
+              text: 'XXX公司',
+              link: '/interview/A'
+            },
+          ]
+          // link: '/javascript/prototype/'
+        }
+      ]
+    }, // 侧边栏配置
+    sidebarDepth: 2, // 侧边栏显示2级
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,15 @@
-# Hello VuePress 2221
-
-## hello
+---
+home: true
+lang: zh-CN
+heroText: 此间少年
+heroImage: /icon.jpeg
+actions:
+  - text: 开始 →
+    link: /javascript/prototype/
+    type: primary
+features:
+  - title: 简介
+    details: 一名普通的前端开发工程师。
+  - title: 寄语
+    details: 把每一件平凡的事情做好就是不平凡。
+---
