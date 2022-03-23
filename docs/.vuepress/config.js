4f590fdfbdb480ad6b0f5072c3ce8920543387ef
戴鹏鹏 <pengpeng.dai@china.zhaogang.com>
2022-03-23 11:51:34 +0800

    fix: commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,8 +11,14 @@ module.exports = {
   themeConfig: {
     navbar:[ // 导航栏配置
       {text: 'JavaScript', link: '/javascript/prototype/' },
-      {text: 'Vue', link: '/vue/'},
-      {text: 'React', link: '/react/'}      
+      {
+        text: 'Vue源码分析',
+        children: [
+          { text: 'vue2.0', link: '/vue/' },
+          {text: 'vue3.0', link: '/vueNext/'}
+        ]
+      },
+      {text: 'React源码分析', link: '/react/'}      
     ],
     sidebar: {
       '/javascript/': [
@@ -30,16 +36,54 @@ module.exports = {
           ],
         }
       ],
-      '/interview/': [
+      '/interviewCompany/': [
         {
           text: '面试记录',
           children: [
             {
               text: 'XXX公司',
-              link: '/interview/A'
+              link: '/interviewCompany/company1'
             },
-          ]
-          // link: '/javascript/prototype/'
+            {
+              text: '2公司',
+              link: '/interviewCompany/company2'
+            },
+          ],
+        }
+      ],
+      '/interviewQuestion/': [
+        {
+          text: '常见面试题',
+          children: [
+            {
+              text: 'HTML',
+              link: '/interviewQuestion/html'
+            },
+            {
+              text: 'CSS',
+              link: '/interviewQuestion/css'
+            },
+            {
+              text: 'JavaScript',
+              link: '/interviewQuestion/javascript'
+            },
+            {
+              text: 'Vue',
+              link: '/interviewQuestion/vue'
+            },
+            {
+              text: 'React',
+              link: '/interviewQuestion/react'
+            },
+            {
+              text: 'Webpack',
+              link: '/interviewQuestion/webpack'
+            },
+            {
+              text: 'HTTP',
+              link: '/interviewQuestion/http'
+            },
+          ],
         }
       ]
     }, // 侧边栏配置
